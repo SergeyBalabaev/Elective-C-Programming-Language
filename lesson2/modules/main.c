ae235f1c6b736956ec758b8cb213ee4718e4c55f
SergeyBalabaev <62759846+SergeyBalabaev@users.noreply.github.com>
2021-04-05 23:54:13 +0300

    Create main.c

---
+++ lesson2/modules/main.c
@@ -0,0 +1,13 @@
+//#include "stdio.h"
+#include "lib.h"
+#define X 0
+int main()
+{
+	int sum = X;
+	summ(10,10,&sum);
+	if(sum>10){
+	    mult(10,10,&sum);
+	}
+//	printf("Res = %d\n", sum);
+	return 0;
+}
