e1962d82f5ac59dd0eb0efc588fd2b51bc3b8805
SergeyBalabaev <62759846+SergeyBalabaev@users.noreply.github.com>
2022-05-16 22:01:59 +0300

    Create enum.c

---
+++ lesson7/enum.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+enum Numbers
+{
+	Zero,
+	One,
+	Two,
+	Three,
+	Four
+};
+int main(void)
+{
+	printf("Zero = %d\n", Zero);
+	printf("One = %d\n", One);
+	printf("Two = %d\n", Two);
+	printf("Three = %d\n", Three);
+	printf("Four = %d\n", Four);
+	return 0;
+}
